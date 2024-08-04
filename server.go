f30c0bbbf7798ced5ff59bf3791d7d96bbf521d9
Hosi121 <takuyakataiwa@icloud.com>
2024-08-05 02:48:46 +0900

    finish tutorial

--- server.go
+++ server.go
@@ -1,10 +1,15 @@
 package main
 
 import (
-	"example/graph/graph"
+	"context"
+	"example/ent"
+	"example/resolver"
 	"log"
 	"net/http"
 	"os"
+
+	"entgo.io/ent/dialect"
+	_ "modernc.org/sqlite"
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
@@ -13,12 +18,37 @@ import (
 const defaultPort = "8080"
 
 func main() {
+	// Create an ent.Client with in-memory SQLite database.
+	entOptions := []ent.Option{}
+	entOptions = append(entOptions, ent.Debug())
+	client, err := ent.Open(dialect.SQLite, "file::memory:?cache=shared", entOptions...)
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+	// Run the automatic migration tool to create all schema resources.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	ctx := context.Background()
+	u, err := client.Todo.
+		Create().
+		SetName("John").
+		SetEmail("hogehoge@gmail.com").
+		SetDone(false).
+		Save(ctx)
+	if err != nil {
+		log.Fatalf("failed creating user: %v", err)
+	}
+	log.Println("user was created: ", u)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = defaultPort
 	}
 
-	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	srv := handler.NewDefaultServer(resolver.NewSchema(client))
 
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
