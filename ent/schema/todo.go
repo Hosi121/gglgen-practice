6296b5bdb12e93a686c3c31e21ae7c2c647db634
Hosi121 <takuyakataiwa@icloud.com>
2024-08-05 02:27:03 +0900

    feat: gglgenを実行

--- ent/schema/todo.go
+++ ent/schema/todo.go
@@ -1,6 +1,11 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"entgo.io/contrib/entgql"
+	"entgo.io/ent"
+	"entgo.io/ent/schema"
+	"entgo.io/ent/schema/field"
+)
 
 // Todo holds the schema definition for the Todo entity.
 type Todo struct {
@@ -9,10 +14,24 @@ type Todo struct {
 
 // Fields of the Todo.
 func (Todo) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("name"),
+		field.String("email").
+			NotEmpty().
+			MaxLen(255),
+		field.Bool("done").
+			Default(false),
+	}
 }
 
 // Edges of the Todo.
 func (Todo) Edges() []ent.Edge {
 	return nil
+}
+
+func (Todo) Annotations() []schema.Annotation {
+	return []schema.Annotation{
+		entgql.QueryField(),
+		entgql.Mutations(entgql.MutationCreate(), entgql.MutationUpdate()),
+	}
 }
